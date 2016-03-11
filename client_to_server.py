3782d3774aa419c3908f37a37c6b8329202d5466
miaoihan <izhanghan@outlook.com>
2016-03-11 14:17:38 +0800

    connect to sina

--- client_to_server.py
+++ client_to_server.py
@@ -1,1 +1,31 @@
-client_to_server
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import socket
+
+#  创建socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#   建立连接
+s.connect(('www.sina.com', 80))
+#  发送数据
+s.send(b'GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnection: close\r\n\r\n')
+
+buffer = []
+while True:
+	#接受1k字节
+	d = s.recv(1024)
+	if d:
+		buffer.append(d)
+	else:
+		break
+data =  b' '.join(buffer)
+
+# 关闭连接:
+s.close()
+
+header, html = data.split(b'\r\n\r\n', 1)
+print('******* 连接成功！ ********')
+print(header.decode('utf-8'))
+# 把接收的数据写入文件:
+with open('sina.html', 'wb') as f:
+    f.write(html)
