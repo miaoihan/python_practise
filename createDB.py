ae45581a1e2c96543c862b2a583e1b344d37c814
miaoihan <izhanghan@outlook.com>
2016-03-22 21:38:34 +0800

    some

--- createDB.py
+++ createDB.py
@@ -9,41 +9,36 @@ from sqlalchemy.ext.declarative import declarative_base
 # # 初始化数据库连接:
 SQL_URL = 'mysql+pymysql://root:1234@localhost/pythondb'
 engine = create_engine(SQL_URL, echo=True)
-# print('连接成功')
-# # 创建DBSession类型:
-# DBSession = sessionmaker(bind=engine)
-# # session = DBSession()
-# # new_user = Han(id='5', name='Bob')
-# # session.add(new_user)
-# # session.commit()
-# # session.close()
-# print('创建成功')
-
+# declarative_base() 创建了一个 BaseModel 类，这个类的子类可以自动与一个表关联。
+BaseModel = declarative_base()
 
 #------------- 数据库表映射 ----------------
 
 class User(BaseModel):
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True)
     name = Column(CHAR(30)) # or Column(String(30))
 
-class UserData(BaseModel):
-    __tablename__ = 'users'
 
+class UserData(BaseModel):
+    __tablename__ = 'usersData'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30)) 
+    data = Column(String(30)) 
+
+
+class haha(BaseModel):
+    __tablename__ = 'haha'
     id = Column(Integer, primary_key=True)
     name = Column(String(30)) 
     data = Column(String(30)) 
 #--------- 数据库表映射------ END ----------------
 
-BaseModel = declarative_base()
+
 def init_db():
     BaseModel.metadata.create_all(engine)
 def drop_db():
     BaseModel.metadata.drop_all(engine)
 
-class User(BaseModel):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(CHAR(30)) # or Column(String(30))
+# 创建表
 init_db()
